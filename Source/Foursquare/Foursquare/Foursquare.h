a70abb880108b6c47776e17163700cd4ab6b7eff
Constantine Fry <constantine.fry@gmail.com>
2014-10-26 13:05:26 +0100

    Create projects and workspace

---
+++ Source/Foursquare/Foursquare/Foursquare.h
@@ -0,0 +1,19 @@
+//
+//  Foursquare.h
+//  Foursquare
+//
+//  Created by Constantine Fry on 26/10/14.
+//  Copyright (c) 2014 Constantine Fry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Foursquare.
+FOUNDATION_EXPORT double FoursquareVersionNumber;
+
+//! Project version string for Foursquare.
+FOUNDATION_EXPORT const unsigned char FoursquareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Foursquare/PublicHeader.h>
+
+
