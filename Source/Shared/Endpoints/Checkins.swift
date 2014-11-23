77dd8929b0603941998fb162f94bb1631802ab3f
Constantine Fry <constantine.fry@gmail.com>
2014-11-23 17:09:43 +0100

    Add checkins endpoint

--- Source/Shared/Endpoints/Checkins.swift
+++ Source/Shared/Endpoints/Checkins.swift
@@ -10,19 +10,64 @@ import Foundation
 
 public class Checkins: Endpoint {
     
+    override var endpoint   : String {
+        return "checkins"
+    }
+    
+    public func get(checkinId: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        return self.getWithPath(checkinId, parameters: nil, completionHandler)
+    }
+    
     // MARK: - General
     
-        // add
-        // recent
+    // add
+    public func add(venueId: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = "add"
+        var allParameters = [Parameter.venueId:venueId]
+        allParameters += parameters
+        return self.postWithPath(path, parameters: allParameters, completionHandler)
+    }
+
+    // recent
+    public func recent(parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = "recent"
+        return self.getWithPath(path, parameters: parameters, completionHandler)
+    }
+
     
     // MARK: - Aspects
     
-        // likes
+    // likes
+    public func likes(checkinId: String, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = checkinId + "/likes"
+        return self.getWithPath(path, parameters: nil, completionHandler)
+    }
     
     // MARK: - Actions
     
-        // addcomment
-        // addpost
-        // deletecomment
-        // like
+    // addcomment
+    public func addcomment(checkinId: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = checkinId + "/addcomment"
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
+    
+    // addpost
+    public func addpost(checkinId: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = checkinId + "/addpost"
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
+    
+    // deletecomment
+    public func deletecomment(checkinId: String, commentId: String, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = checkinId + "/deletecomment"
+        let parameters = [Parameter.commentId:commentId]
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
+    
+    // like
+    public func like(checkinId: String, like: Bool, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = checkinId + "/like"
+        let parameters = [Parameter.set: (like) ? "1":"0"]
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
 }
