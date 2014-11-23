d941c9713f47cd81d4ed7258582b8d5a5b436a8a
Constantine Fry <constantine.fry@gmail.com>
2014-11-23 16:58:34 +0100

    Fix venues endpoint and add Venue groups end point

--- Source/Shared/Endpoints/VenueGroups.swift
+++ Source/Shared/Endpoints/VenueGroups.swift
@@ -9,22 +9,70 @@
 import Foundation
 
 public class VenueGroups: Endpoint {
+    override var endpoint   : String {
+        return "venuegroups"
+    }
+    
+    public func get(groupId: String, completionHandler:  ResponseClosure? = nil) -> Task {
+        return self.getWithPath(groupId, parameters: nil, completionHandler)
+    }
     
     // MARK: - General
     
-        // add
-        // delete
-        // list
+    // add
+    public func add(name: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = "add"
+        var allParameters = [Parameter.name: name]
+        allParameters += parameters
+        return self.postWithPath(path, parameters: allParameters, completionHandler)
+    }
+    
+    // delete
+    public func delete(groupId: String, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = groupId + "/delete"
+        return self.postWithPath(path, parameters: nil, completionHandler)
+    }
+    
+    // list
+    public func list(completionHandler: ResponseClosure? = nil) -> Task {
+        let path = "list"
+        return self.getWithPath(path, parameters: nil, completionHandler)
+    }
     
     // MARK: - Aspects
     
-        // timeseries
+    // timeseries
+    public func timeseries(groupId: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = groupId + "/timeseries"
+        return self.getWithPath(path, parameters: parameters, completionHandler)
+    }
     
     // MARK: - Actions
     
-        // addvenue
-        // edit
-        // removevenue
-        // update
+    // addvenue
+    public func addvenue(groupId: String, venueId: [String], completionHandler: ResponseClosure? = nil) -> Task {
+        let path = groupId + "/addvenue"
+        let parameters = [Parameter.venueId:join(",", venueId)]
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
+    
+    // edit
+    public func edit(groupId: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = groupId + "/edit"
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
+    
+    // removevenue
+    public func removevenue(groupId: String,  venueId: [String], completionHandler: ResponseClosure? = nil) -> Task {
+        let path = groupId + "/removevenue"
+        let parameters = [Parameter.venueId:join(",", venueId)]
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
+    
+    // update
+    public func update(groupId: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
+        let path = groupId + "/update"
+        return self.postWithPath(path, parameters: parameters, completionHandler)
+    }
     
 }
--- Source/Shared/Endpoints/Venues.swift
+++ Source/Shared/Endpoints/Venues.swift
@@ -64,80 +64,82 @@ public class Venues: Endpoint {
     // trending
     public func trending(ll:String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
         let path = "trending"
-        return self.getWithPath(path, parameters: parameters, completionHandler)
+        var allParameters = [Parameter.ll:ll]
+        allParameters += parameters
+        return self.getWithPath(path, parameters: allParameters, completionHandler)
     }
     
     // MARK: - Aspects
     
     // events
     public func events(venuId:String, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "events"
+        let path = venuId + "/events"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
     // herenow
     public func herenow(venuId:String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "herenow"
+        let path = venuId + "/herenow"
         return self.getWithPath(path, parameters: parameters, completionHandler)
     }
     
     // hours
     public func hours(venuId:String, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "hours"
+        let path = venuId + "/hours"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
     // likes
     public func likes(venuId:String, completionHandler:  ResponseClosure? = nil) -> Task {
-        let path = "likes"
+        let path = venuId + "/likes"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
     // links
     public func links(venuId:String, completionHandler:  ResponseClosure? = nil) -> Task {
-        let path = "links"
+        let path = venuId + "/links"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
     // listed
     public func listed(venuId:String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "listed"
+        let path = venuId + "/listed"
         return self.getWithPath(path, parameters: parameters, completionHandler)
     }
     
     // menu
     public func menu(venuId:String, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "menu"
+        let path = venuId + "/menu"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
     // nextvenues
     public func nextvenues(venuId:String, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "nextvenues"
+        let path = venuId + "/nextvenues"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
     // photos
     public func photos(venuId:String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "photos"
+        let path = venuId + "/photos"
         return self.getWithPath(path, parameters: parameters, completionHandler)
     }
     
     // similar
     public func similar(venuId:String, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "similar"
+        let path = venuId + "/similar"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
     // stats
     public func stats(venuId:String,  parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "stats"
+        let path = venuId + "/stats"
         return self.getWithPath(path, parameters: parameters, completionHandler)
     }
     
     // tips
     public func tips(venuId:String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "tips"
+        let path = venuId + "/tips"
         return self.getWithPath(path, parameters: parameters, completionHandler)
     }
     
@@ -145,27 +147,27 @@ public class Venues: Endpoint {
     
     // claim
     public func claim(venuId:String, visible: Bool, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "claim"
+        let path = venuId + "/claim"
         let parameters = [Parameter.visible: (visible) ? "true":"false"]
         return self.postWithPath(path, parameters: parameters, completionHandler)
     }
     
     // dislike
     public func dislike(venuId:String, dislike: Bool, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "dislike"
+        let path = venuId + "/dislike"
         let parameters = [Parameter.set: (dislike) ? "1":"0"]
         return self.postWithPath(path, parameters: parameters, completionHandler)
     }
     
     // edit
     public func edit(venuId:String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "edit"
+        let path = venuId + "/edit"
         return self.postWithPath(path, parameters: parameters, completionHandler)
     }
     
     // flag
     public func flag(venuId:String, problem: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "flag"
+        let path = venuId + "/flag"
         var allParameters = [Parameter.problem:problem]
         allParameters += parameters
         return self.postWithPath(path, parameters: allParameters, completionHandler)
@@ -173,20 +175,20 @@ public class Venues: Endpoint {
     
     // like
     public func like(venuId:String, like: Bool, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "like"
+        let path = venuId + "/like"
         let parameters = [Parameter.set: (like) ? "1":"0"]
         return self.postWithPath(path, parameters: parameters, completionHandler)
     }
     
     // proposeedit
     public func proposeedit(venuId:String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "proposeedit"
+        let path = venuId + "/proposeedit"
         return self.postWithPath(path, parameters: parameters, completionHandler)
     }
     
     // setrole
     public func setrole(venuId:String, userId: String, role: String, parameters: Parameters?, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "setrole"
+        let path = venuId + "/setrole"
         var allParameters = [Parameter.userId:userId, Parameter.role:role]
         allParameters += parameters
         return self.postWithPath(path, parameters: allParameters, completionHandler)
@@ -194,7 +196,7 @@ public class Venues: Endpoint {
     
     // setsinglelocation
     public func setsinglelocation(venuId:String, completionHandler: ResponseClosure? = nil) -> Task {
-        let path = "setsinglelocation"
+        let path = venuId + "/setsinglelocation"
         return self.getWithPath(path, parameters: nil, completionHandler)
     }
     
