0a8ca8d90213dd56678893b8059d6aa0fe8187b5
Constantine Fry <constantine.fry@gmail.com>
2015-08-24 21:15:39 +0200

    Change tint color of the main window.

--- Demo/Demo-iOS/Demo-iOS/AppDelegate.swift
+++ Demo/Demo-iOS/Demo-iOS/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
             
+            self.window?.tintColor = UIColor(red: 71.0/255.0, green: 57.0/255.0, blue: 151.0/255.0, alpha: 1.0)
+            
             let client = Client(clientID:       "5P1OVCFK0CCVCQ5GBBCWRFGUVNX5R4WGKHL2DGJGZ32FDFKT",
                 clientSecret:   "UPZJO0A0XL44IHCD1KQBMAYGCZ45Z03BORJZZJXELPWHPSAR",
                 redirectURL:    "testapp123://foursquare")
